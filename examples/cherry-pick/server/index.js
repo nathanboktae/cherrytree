17612ee2bcbf45b56ee25a1a25d5dbcac1f2bf1d
Karolis Narkevicius <karolis.n@gmail.com>
2015-05-17 22:53:14 +0100

    Deploy cherry-pick demo using pm2

---
+++ examples/cherry-pick/server/index.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+app.use(express.static(path.join(__dirname, '..', 'dist')))
+app.use(express.static(path.join(__dirname, '..', 'public')))
+app.listen(4400, function () {
+  console.log('cheery-pick demo started on http://localhost:4400')
+})
